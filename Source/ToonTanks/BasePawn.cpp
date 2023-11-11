13a7456b8a312864a42828608fdc7062381cf6c2
Gian <gianluca.fiorinigmail.com>
2023-11-11 13:10:45 +0100

    BP_TurretPawn

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -1,34 +1,33 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
-ABasePawn::ABasePawn()
-{
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ABasePawn::ABasePawn() {
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Component"));
+	BaseMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh Component"));
+	TurretComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh Component"));
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Spawn Point Component"));
+
+	RootComponent = CapsuleComponent;
+	BaseMeshComponent->SetupAttachment(RootComponent);
+	TurretComponent->SetupAttachment(BaseMeshComponent);
+	ProjectileSpawnPoint->SetupAttachment(TurretComponent);
 }
 
 // Called when the game starts or when spawned
-void ABasePawn::BeginPlay()
-{
+void ABasePawn::BeginPlay() {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
-void ABasePawn::Tick(float DeltaTime)
-{
+void ABasePawn::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
-void ABasePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
+void ABasePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
-
--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -7,8 +7,7 @@
 #include "BasePawn.generated.h"
 
 UCLASS()
-class TOONTANKS_API ABasePawn : public APawn
-{
+class TOONTANKS_API ABasePawn : public APawn {
 	GENERATED_BODY()
 
 public:
@@ -19,11 +18,23 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+private:
+	UPROPERTY(VisibleAnywhere)
+	class UCapsuleComponent* CapsuleComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* BaseMeshComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* TurretComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* ProjectileSpawnPoint;
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
